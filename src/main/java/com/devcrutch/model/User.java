6c69fbf84b1112e56b14483b948dec1b9233ba41
vahidhashemi <svahid.hashemi@gmail.com>
2018-12-07 10:41:41 +0330

    Changed user model

--- src/main/java/com/devcrutch/model/User.java
+++ src/main/java/com/devcrutch/model/User.java
@@ -11,20 +11,42 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private boolean isAdmin;
 
-    public User(String username, String password) {
+    public User(String username, String password, boolean isAdmin) {
         this.username = username;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
     public User() {
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
