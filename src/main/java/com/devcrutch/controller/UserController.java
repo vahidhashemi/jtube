a3aa036cc63c78adb14e755b2b9805c3e14807e5
vahidhashemi <svahid.hashemi@gmail.com>
2018-12-07 10:47:40 +0330

    rectified api call

--- src/main/java/com/devcrutch/controller/UserController.java
+++ src/main/java/com/devcrutch/controller/UserController.java
@@ -17,10 +17,28 @@ public class UserController {
         return userRepository.saveAndFlush(user);
     }
 
-    @RequestMapping(value = "users/{id}/password", method = RequestMethod.PUT)
+    @PutMapping(value = "users/{id}/password")
     public User changePassword(@PathVariable Long id, @RequestBody String password) {
         User existingUser = userRepository.findById(id).get();
         existingUser.setPassword(password);
         return userRepository.saveAndFlush(existingUser);
     }
+
+    @PutMapping(value = "users/{id}/promote")
+    public User prompoteUser(@PathVariable Long id) {
+        return makeUserAdmin(id, true);
+    }
+
+    @PutMapping(value = "users/{id}/demote")
+    public User demoteUser(@PathVariable Long id) {
+        return makeUserAdmin(id, false);
+    }
+
+
+    private User makeUserAdmin(Long id, boolean isAdmin) {
+        User existingUser = userRepository.findById(id).get();
+        existingUser.setAdmin(isAdmin);
+        return userRepository.saveAndFlush(existingUser);
+    }
+
 }
